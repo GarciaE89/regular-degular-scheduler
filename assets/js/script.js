0dfecff801842a9e1164d66042770045d8664cfd
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-08-29 13:59:11 -0600

    added hour elment, class name and time div, appended to column

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,18 @@ var currentDayEl = document.querySelector("#currentDay");
 
 var currentTime = moment();
 
-currentDayEl.append(currentTime.format('MMMM Do YYYY, h:mm:ss a'));
+currentDayEl.append(currentTime.format("MMMM Do YYYY"));
 
 var containerEl = document.querySelector(".container");
 
 var addTimeBlocks = function(hour){
     var timeBlockEl = document.createElement("div");
     timeBlockEl.className = "row time-block";
-    
+
+    var hourColumnEl = document.createElement("div");
+    hourColumnEl.className = "col-sm-2 p-0";
+    var timeDiv = document.createElement("div");
+    timeDiv.className = "hour";
+    timeDiv.textContent = moment(hour, "HH".format("h:mm A"));
+    hourColumn.append(timeDiv);
 }
