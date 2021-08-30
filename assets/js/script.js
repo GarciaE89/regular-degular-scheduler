5f1788b0204b95428103dc7a29ac4ab7aed3a7a1
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-08-30 08:00:03 -0600

    added text col, save col, appended to container el, and save function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-var currentDayEl = document.querySelector("#currentDay");
+var currentDayEl = document.getElementById("currentDay");
 
 var currentTime = moment();
 
-currentDayEl.append(currentTime.format("MMMM Do YYYY"));
+currentDayEl.append(currentTime.format("MMMM Do, YYYY"));
 
-var containerEl = document.querySelector(".container");
+var containerEl = document.getElementById("schedule-time-blocks");
 
 var addTimeBlocks = function(hour){
     var timeBlockEl = document.createElement("div");
@@ -14,6 +14,43 @@ var addTimeBlocks = function(hour){
     hourColumnEl.className = "col-sm-2 p-0";
     var timeDiv = document.createElement("div");
     timeDiv.className = "hour";
-    timeDiv.textContent = moment(hour, "HH".format("h:mm A"));
-    hourColumn.append(timeDiv);
+    timeDiv.textContent = moment(hour, "HH").format("h:mm A");
+    hourColumnEl.append(timeDiv);
+
+    var textColumnEl = document.createElement("div");
+    textColumnEl.className = "col-sm-8 p-0";
+    var textArea = document.createElement("textarea");
+    textArea.className = "description";
+    textArea.textContent = localStorage.getItem(hour);
+    if(hour < currentTime.hour()) {
+        textColumnEl.classList.add("past")
+    }
+    else if (hour > currentTime.hour()){
+        textColumnEl.classList.add("future")
+    }
+    else {
+        textColumnEl.classList.add("present");
+    }
+    textColumnEl.append(textArea);
+    
+    var saveColumnEl = document.createElement("div");
+    saveColumnEl.className = "col-sm-2 p-0";
+    var saveBtn = document.createElement("button");
+    saveBtn.className = "saveBtn";
+    var saveIconEl = document.createElement("i");
+    saveIconEl.className = "far fa-save";
+    saveBtn.append(saveIconEl);
+    saveColumnEl.append(saveBtn);
+
+    saveBtn.addEventListener("click", function (){
+        localStorage.setItem(hour, textArea.value);
+    });
+        
+      timeBlockEl.append(hourColumnEl, textColumnEl, saveColumnEl);
+      containerEl.append(timeBlockEl);
+    
 }
+ for (var i = 9; i < 18; i++) {
+     addTimeBlocks(i)
+     
+ }
